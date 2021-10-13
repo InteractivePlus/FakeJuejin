551d81db1f515d956a8041b36182f78a1055292b
Liuxing <coopticks@outlook.com>
2021-10-13 18:38:44 +0800

    Second

--- pages/index.js
+++ pages/index.js
@@ -1,82 +1,45 @@
 import Head from 'next/head'
 
 export default function Home() {
+  const Nav = () => (
+    <div className="sticky top-0  w-full mx-auto bg-white border-b border-gray-200 flex-none flex">
+      <div className="flex-none flex mx-auto p-5">
+        <div className="flex-none items-center mr-5">
+          <img className="w-auto h-6" src="//lf3-cdn-tos.bytescm.com/obj/static/xitu_juejin_web/7abc2b532f725d394feaf0141547ade7.svg" />
+        </div>
+        <div className="flex-auto flex items-center ml-3">
+          <ul className="flex-grow flex text-juejinnav">
+            <li className="flex-1 px-5">
+              <a className="cursor-pointer">
+                首页
+              </a>
+            </li>
+
+            <li className="flex-1 px-5">
+              <a className="cursor-pointer">
+                沸点
+              </a>
+            </li>
+          </ul>
+        </div>
+
+      </div>
+    </div>
+
+  )
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+    <div className="overflow-hidden font-juejin">
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
+      <Nav />
+      <div className="min-h-screen  mx-auto bg-juejinbg"></div>
 
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="p-3 font-mono text-lg bg-gray-100 rounded-md">
-            pages/index.js
-          </code>
-        </p>
 
-        <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className="flex items-center justify-center w-full h-24 border-t">
-        <a
-          className="flex items-center justify-center"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="h-4 ml-2" />
-        </a>
-      </footer>
     </div>
   )
+
+
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,26 @@
+
+const colors = require('tailwindcss/colors')
+
+
 module.exports = {
   mode: 'jit',
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
+    colors: {
+      // Build your palette here
+      transparent: 'transparent',
+      juejinbg: '#f4f5f5',
+      juejinnav: '#86909c',
+      gray: colors.trueGray,
+      red: colors.red,
+      blue: colors.sky,
+      yellow: colors.amber,
+    },
+    fontFamily: {
+      'juejin': ["-apple-system","system-ui","Segoe UI","Roboto","Ubuntu","Cantarell","Noto Sans","sans-serif","BlinkMacSystemFont","Helvetica Neue","PingFang SC","Hiragino Sans GB","Microsoft YaHei","Arial"]
+    }
   },
   variants: {
     extend: {},
