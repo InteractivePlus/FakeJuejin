e54feceb76c85b50fd6492b39e7829a82df078fe
wey <weypro@users.noreply.github.com>
2021-10-15 19:26:01 +0800

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,27 +3,47 @@ const colors = require('tailwindcss/colors')
 
 
 module.exports = {
-  mode: 'jit',
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {},
-    colors: {
-      // Build your palette here
-      transparent: 'transparent',
-      juejinbg: '#f4f5f5',
-      juejinnav: '#86909c',
-      gray: colors.trueGray,
-      red: colors.red,
-      blue: colors.sky,
-      yellow: colors.amber,
+    mode: "jit",
+    purge: [
+        "./pages/**/*.{js,ts,jsx,tsx}",
+        "./components/**/*.{js,ts,jsx,tsx}",
+    ],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        extend: {
+            colors: {
+              // Build your palette here
+              transparent: 'transparent',
+              juejinbg: '#f4f5f5',
+              juejinnav: '#86909c',
+              gray: colors.trueGray,
+              red: colors.red,
+              blue: colors.sky,
+              yellow: colors.amber,
+            },
+        },
+
+        fontFamily: {
+            juejin: [
+                "-apple-system",
+                "system-ui",
+                "Segoe UI",
+                "Roboto",
+                "Ubuntu",
+                "Cantarell",
+                "Noto Sans",
+                "sans-serif",
+                "BlinkMacSystemFont",
+                "Helvetica Neue",
+                "PingFang SC",
+                "Hiragino Sans GB",
+                "Microsoft YaHei",
+                "Arial",
+            ],
+        },
     },
-    fontFamily: {
-      'juejin': ["-apple-system","system-ui","Segoe UI","Roboto","Ubuntu","Cantarell","Noto Sans","sans-serif","BlinkMacSystemFont","Helvetica Neue","PingFang SC","Hiragino Sans GB","Microsoft YaHei","Arial"]
-    }
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
-}
+    variants: {
+        extend: {},
+    },
+    plugins: [],
+};
